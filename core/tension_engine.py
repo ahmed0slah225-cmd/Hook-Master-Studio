2c6d97d31d505aa97d99ab5ac9425f46490206eb
ahmed0slah225-cmd <ahmed0slah225@gmail.com>
2026-09-15 19:17:39 +0300

    Delete project files

--- core/tension_engine.py
+++
@@ -1,8 +0,0 @@
-class TensionEngine:
-    def build(self, analysis):
-        return {
-            "problem": analysis.core_problem,
-            "conflict": analysis.conflicts[0] if analysis.conflicts else "",
-            "promise": analysis.promise,
-            "transformation": analysis.transformation,
-        }

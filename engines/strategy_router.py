b6e88f69dd20d15987bf9679fda1517ecf4c6c97
ahmed0slah225-cmd <ahmed0slah225@gmail.com>
2026-09-15 19:18:46 +0300

    Delete project files

--- engines/strategy_router.py
+++
@@ -1,8 +0,0 @@
-from config.constants import HOOK_TYPES
-class StrategyRouter:
-    """Maps content conditions to complementary hook strategies."""
-    def route(self, analysis):
-        routes = list(HOOK_TYPES)
-        if analysis.conflicts: routes.insert(0, "conflict")
-        if analysis.strongest_moments: routes.insert(0, "story")
-        return list(dict.fromkeys(routes))
